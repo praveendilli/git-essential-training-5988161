1d3d2b4b639fa5947edb42a3d1c8aecc0f692a15
praveendilli <rpraveendilli@gmail.com>
2026-07-18 11:54:36 +0000

    finalized

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
-Print("Student Management System")
+print("Student Management System")
+students=[]
+n=int(input("Enter no. of. entries: "))
+for i in range(n):
+  j=input("Enter name: ")
+  k=int(input("Student ID: "))
+  l=float(input("Enter CGPA: "))
+  students.append([j,k,l])
+
+print(students)
